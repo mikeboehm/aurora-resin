8e37e3508a5a46deab269d153ef6d26c77b9c461
Mike Boehm <mike@boehm.com.au>
2015-07-24 22:42:35 +0100

    environmental vars

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Device ID: ' + os.environ['RESIN_DEVCICE_UUID']
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080, debug='true')

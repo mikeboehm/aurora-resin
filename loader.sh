1c6e22e2c1c0560372095225d7178a0f55d6f719
Mike Boehm <mike@boehm.com.au>
2015-07-31 22:26:37 +0100

    Commit

--- loader.sh
+++ loader.sh
@@ -2,11 +2,12 @@
 
 # git --git-dir=/app/.git/ --work-tree=/app/ submodule init
 # git --git-dir=/app/.git/ --work-tree=/app/ submodule update
-git clone https://github.com/mikeboehm/aurora-web.git /app/aurora-web
-git clone https://github.com/mikeboehm/Aurora.git /app/Aurora
-source /app/Aurora/install_dependencies.sh
+# git clone https://github.com/mikeboehm/aurora-web.git /app/aurora-web
+# git clone https://github.com/mikeboehm/Aurora.git /app/Aurora
+# source /app/Aurora/install_dependencies.sh
 
 lifx-http &
-sudo python /app/Aurora/aurora_exec.py &
+# sudo python /app/Aurora/aurora_exec.py &
+# python /app/aurora-web/server.py &
 
 exit 0
